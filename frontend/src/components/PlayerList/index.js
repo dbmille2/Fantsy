2490ba9797e9d712b364b35a38ca504a9927ddc9
Daniel Miller <dbmiller25@gmail.com>
2021-01-01 07:28:29 -0500

    Player sorting on team page

--- frontend/src/components/PlayerList/index.js
+++ frontend/src/components/PlayerList/index.js
@@ -5,50 +5,97 @@ import PlayerCard from "../PlayerCard";
 function PlayerList({ selection, subSelection }) {
   const followedPlayers = useSelector((state) => state.session.followedPlayers);
   const allPlayers = useSelector((state) => state.players.allPlayers);
+  let allPlayersArr = Object.values(allPlayers);
+  let followedPlayersArr = Object.values(followedPlayers);
+  allPlayersArr.forEach((player) => {
+    switch (player.position) {
+      case "QB":
+        player.value = 1;
+        break;
+      case "RB":
+        player.value = 2;
+        break;
+      case "WR":
+        player.value = 3;
+        break;
+      case "TE":
+        player.value = 4;
+        break;
+      case "K":
+        player.value = 5;
+        break;
+      default:
+        break;
+    }
+  });
+  followedPlayersArr.forEach((player) => {
+    switch (player.position) {
+      case "QB":
+        player.value = 1;
+        break;
+      case "RB":
+        player.value = 2;
+        break;
+      case "WR":
+        player.value = 3;
+        break;
+      case "TE":
+        player.value = 4;
+        break;
+      case "K":
+        player.value = 5;
+        break;
+      default:
+        break;
+    }
+  });
+  allPlayersArr.sort((a, b) => (a.value > b.value ? 1 : -1));
+  followedPlayersArr.sort((a, b) => (a.value > b.value ? 1 : -1));
+
   return (
     <ul className="player-list">
       {selection === "all" &&
         subSelection === "all" &&
-        Object.values(allPlayers).map((player) => {
+        allPlayersArr.map((player) => {
           return <PlayerCard key={player.id} player={player} />;
         })}
       {selection === "all" &&
         subSelection === "QB" &&
-        Object.values(allPlayers)
+        allPlayersArr
           .filter((player) => player.position === "QB")
           .map((player) => {
             return <PlayerCard key={player.id} player={player} />;
           })}
       {selection === "all" &&
         subSelection === "RB" &&
-        Object.values(allPlayers)
+        allPlayersArr
           .filter((player) => player.position === "RB")
           .map((player) => {
             return <PlayerCard key={player.id} player={player} />;
           })}
       {selection === "all" &&
         subSelection === "WR" &&
-        Object.values(allPlayers)
+        allPlayersArr
           .filter((player) => player.position === "WR")
           .map((player) => {
             return <PlayerCard key={player.id} player={player} />;
           })}
       {selection === "all" &&
         subSelection === "TE" &&
-        Object.values(allPlayers)
+        allPlayersArr
           .filter((player) => player.position === "TE")
           .map((player) => {
             return <PlayerCard key={player.id} player={player} />;
           })}
       {selection === "all" &&
         subSelection === "K" &&
-        Object.values(allPlayers)
+        allPlayersArr
           .filter((player) => player.position === "K")
           .map((player) => {
             return <PlayerCard key={player.id} player={player} />;
           })}
       {selection === "team" &&
-        Object.values(followedPlayers).map((player) => {
+        followedPlayersArr.map((player) => {
           return <PlayerCard key={player.id} player={player} />;
         })}
     </ul>
