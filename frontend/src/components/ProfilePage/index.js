482682491e2532891a68e2f30f856c9eb8ee8edc
Daniel Miller <dbmiller25@gmail.com>
2021-01-04 04:46:26 -0500

    Try to fix

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -16,68 +16,76 @@ function ProfilePage({ tab }) {
   const dispatch = useDispatch();
   const profile = useSelector((state) => state.profile);
   const user = useSelector((state) => state.session.user);
-  const session = useSelector((state) => state.session);
-  const isSelf = profile.isSelf;
-  const profilePic = profile.profilePic;
+  // const session = useSelector((state) => state.session);
+  // const isSelf = profile.isSelf;
+  // const profilePic = profile.profilePic;
   let [month, _date, year] = new Date(profile.createdAt)
     .toLocaleDateString("en-US")
     .split("/");
 
-  useEffect(() => {
-    dispatch(fetchInfo(session.user.username));
-    // ;
-  }, [dispatch, session.user]);
+  // useEffect(() => {
+  //   dispatch(fetchInfo(session.user.username));
+  //   // ;
+  // }, [dispatch, session.user]);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   useEffect(() => {
     dispatch(profileActions.fetchProfile(username, user.id));
-  }, [dispatch, username, user]);
+  }, [dispatch, username, user.id]);
 
   return (
     <div className="full-profile-page">
       <div className="profile-heading">
-        <img className="banner" src={profile.banner} alt="banner" />
+        {profile && (
+          <img className="banner" src={profile.banner} alt="banner" />
+        )}
+        {profile && (
+          <div className="profile-pic-and-follow">
+            <img
+              className="profile-pic"
+              src={profile.profilePic}
+              alt="Profile"
+            />
 
-        <div className="profile-pic-and-follow">
-          {profilePic && (
-            <img className="profile-pic" src={profilePic} alt="Profile" />
-          )}
-          <div className="profile-pic-background"></div>
-          {!isSelf && <FollowButton className="follow" />}
-          {isSelf && <EditProfileButton />}
-        </div>
-        <div className="profile-info-container">
-          <div className="profile-display-name">{profile.displayName}</div>
-          <div className="profile-username">@{profile.username}</div>
-          <div className="profile-bio">{profile.bio}</div>
-          <div className="profile-join-date">
-            <i className="far fa-calendar-alt" />
-            <span>
-              Joined {months[month - 1]} {year}
-            </span>
+            <div className="profile-pic-background"></div>
+            {!profile.isSelf && <FollowButton className="follow" />}
+            {profile.isSelf && <EditProfileButton />}
           </div>
+        )}
+        {profile && (
+          <div className="profile-info-container">
+            <div className="profile-display-name">{profile.displayName}</div>
+            <div className="profile-username">@{profile.username}</div>
+            <div className="profile-bio">{profile.bio}</div>
+            <div className="profile-join-date">
+              <i className="far fa-calendar-alt" />
+              <span>
+                Joined {months[month - 1]} {year}
+              </span>
+            </div>
 
-          <Link className="follow-container" to={`/${username}/following`}>
-            {profile.following && (
-              <span className="follow-number">
-                {Object.keys(profile.following).length}
-              </span>
-            )}
-            <span className="follow-text"> Following</span>
-          </Link>
-          <Link className="follow-container" to={`/${username}/followers`}>
-            {profile.followers && (
-              <span className="follow-number">
-                {Object.keys(profile.followers).length}
-              </span>
-            )}
-            <span className="follow-text"> Followers</span>
-          </Link>
-        </div>
+            <Link className="follow-container" to={`/${username}/following`}>
+              {profile.following && (
+                <span className="follow-number">
+                  {Object.keys(profile.following).length}
+                </span>
+              )}
+              <span className="follow-text"> Following</span>
+            </Link>
+            <Link className="follow-container" to={`/${username}/followers`}>
+              {profile.followers && (
+                <span className="follow-number">
+                  {Object.keys(profile.followers).length}
+                </span>
+              )}
+              <span className="follow-text"> Followers</span>
+            </Link>
+          </div>
+        )}
       </div>
       {profile && <ProfilePostNav />}
-      {profile.id && <ProfileFeedContainer tab={tab} />}
+      {profile && <ProfileFeedContainer tab={tab} />}
     </div>
   );
 }
