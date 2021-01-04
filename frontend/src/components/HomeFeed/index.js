c8d021c73a4276428cf5617b673dd351c4d34a7b
Daniel Miller <dbmiller25@gmail.com>
2021-01-04 04:56:26 -0500

    Fixed saved posts

--- frontend/src/components/HomeFeed/index.js
+++ frontend/src/components/HomeFeed/index.js
@@ -1,12 +1,16 @@
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 import Post from "../Post";
 import "./HomeFeed.css";
 
 function HomeFeed({ user }) {
   const feed = useSelector((state) => state.posts.feed);
+  const savedPosts = useSelector((state) => state.session.savedPosts);
+  useEffect(() => {}, [savedPosts]);
   return (
     <div className="home-feed">
       {feed &&
+        savedPosts &&
         Object.values(feed)
           .sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1))
           .map((post) => {
