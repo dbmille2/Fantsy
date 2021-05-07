f7c365f1f424ecaf069f8b0ca91db3b21d5a681a
Daniel Miller <dbmiller25@gmail.com>
2021-05-06 23:11:30 -0400

    Cleanup API files

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,7 +4,6 @@ const usersRouter = require("./users.js");
 const playersRouter = require("./players.js");
 const postsRouter = require("./posts.js");
 const searchRouter = require("./search.js");
-// GET /api/set-token-cookie
 const asyncHandler = require("express-async-handler");
 const { setTokenCookie } = require("../../utils/auth.js");
 const { User } = require("../../db/models");
@@ -21,21 +20,10 @@ router.get(
   })
 );
 
-// GET /api/restore-user
 const { restoreUser } = require("../../utils/auth.js");
 router.get("/restore-user", restoreUser, (req, res) => {
   return res.json(req.user);
 });
-
-// // GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
