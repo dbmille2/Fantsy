fe3a63797ce98aea26f6a89e7bd2579eb125104a
Daniel Miller <dbmiller25@gmail.com>
2021-05-08 21:57:07 -0400

    Cleanup

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -4,20 +4,17 @@ const { User } = require("../db/models");
 
 const { secret, expiresIn } = jwtConfig;
 
-// Sends a JWT Cookie
 const setTokenCookie = (res, user) => {
-  // Create the token.
   const token = jwt.sign(
     { data: user.toSafeObject() },
     secret,
-    { expiresIn: parseInt(expiresIn) }, // 604,800 seconds = 1 week
+    { expiresIn: parseInt(expiresIn) },
   );
 
   const isProduction = process.env.NODE_ENV === "production";
 
-  // Set the token cookie
   res.cookie("token", token, {
-    maxAge: expiresIn * 1000, // maxAge in milliseconds
+    maxAge: expiresIn * 1000,
     httpOnly: true,
     secure: isProduction,
     sameSite: isProduction && "Lax",
@@ -27,7 +24,6 @@ const setTokenCookie = (res, user) => {
 };
 
 const restoreUser = (req, res, next) => {
-  // token parsed from cookies
   const { token } = req.cookies;
 
   return jwt.verify(token, secret, null, async (err, jwtPayload) => {
